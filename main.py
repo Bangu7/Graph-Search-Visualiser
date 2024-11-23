de6258292085118fa8e15a6ae245a24c57d95c43
Bangu7 <118105318+Bangu7@users.noreply.github.com>
2024-11-23 22:27:05 +1000

    Updated main to animate the graph stepping progression.

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import graph
 
-def update(frame):
-    if frame >= 10:
-        return 
-
-def draw_graph(G: nx.graph) -> None:
+def draw_graph(G: graph.Graph) -> None:
     fig, ax = plt.subplots()
-    nx.draw(G, with_labels=True, font_weight='bold', node_color='skyblue')
-    animation.FuncAnimation(fig=fig, func=update)
+    pos = nx.spring_layout(G._graph)
+
+    def update(frame):
+        if G._solved:
+            return
+        G.step()
+        ax.clear()
+        node_colors = [G._graph.nodes[node].get('color', 'skyblue') for node in G._graph.nodes]
+        nx.draw(G._graph, ax=ax, pos=pos, with_labels=True, node_color=node_colors)
+
+    anim = animation.FuncAnimation(fig=fig, func=update, frames=range(100), blit=False, repeat=False, interval=500)
     plt.show()
 
 def main():
-    G = graph.Graph(nodes=[1, 2, 3, 4], edges=[(1, 2), (2, 3)])
+    G = graph.BFSGraph(nodes=[i for i in range(100)], edges=[(i, i + 1) for i in range(99)], start=5, goal=7)
     draw_graph(G)
 
 if __name__ == "__main__":
